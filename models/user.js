25ccb0741adbd79f5698b8ca63df1b28166f2ee6
Darshilp326 <darshil_1901me18@iitp.ac.in>
2020-05-13 19:37:08 +0530

    code

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create user Schema & model
+const UserSchema = new Schema({
+    name:{
+        type: String
+    },
+    email: {
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password field is required']
+    },
+    phone:{
+        type:String
+      },
+    accessToken: {
+        type: String,
+    }
+    
+});
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
